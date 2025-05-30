30d0cd47465b4a0413b696c2caa4412902e1c473
keerthish waran <durai.s@spotft.com>
2025-05-30 14:45:30 +0530

    add contact us

--- src/components/homepage.jsx
+++ src/components/homepage.jsx
@@ -25,37 +25,11 @@ import DelegatePasses from "./DelegatePasses";
 import VenueInfo from "./VenueInfo";
 import Partners from "./Partners";
 import RegisterNow from "./RegisterNow";
-
+import Header from "./Header";
 const Homepage = () => {
   return (
     <>
-      <div className="contactheader flex items-center justify-between h-10 w-full bg-purple-950">
-        <p className="text-lime-50 pl-6">
-          <span className="text-base font-semibold">Viksit Bharat by 2047 </span>
-          <span className="text-sm font-normal">Innovate 💡, Collaborate 🤝, Celebrate 🎉</span>
-        </p>
-        <div className="contact-us flex items-center">
-          <i className='bx bx-envelope'></i>
-         
-           <Link className="text-lime-50 pr-6 flex items-center"  activeClass="active" to="contact" spy={true} smooth={true} offset={-70} duration={500}>Contact Us</Link>
-        </div>
-      </div>
-
-
-
-
-
-      <div id="header-div" className="flex justify-between w-full bg-white">
-        <div className="logo w-24 h-18">
-          <img src={logo_path} alt="BEF Logo" />
-        </div>
-
-        <nav className="menu flex items-center">
-          <Link activeClass="active" to="event" spy={true} smooth={true} offset={-70} duration={500}>Event</Link>
-          <Link activeClass="active" to="delegate" spy={true} smooth={true} offset={-70} duration={500}>Delegate</Link>
-          <Link activeClass="active" to="section1" spy={true} smooth={true} offset={-70} duration={500}>Home</Link>
-        </nav>
-      </div>
+      <Header/>
 
       {/* Animated Banner */}
       <section id="banner-object" className="mb-12">
